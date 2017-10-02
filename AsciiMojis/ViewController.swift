9c662b58076a0d46c513d4dd5b8ecfb230ce606f
sventropy <sventropy@mailbox.org>
2017-10-02 23:35:46 +0200

    gradient on main screen, similar to app icon

--- AsciiMojis/ViewController.swift
+++ AsciiMojis/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // gradient similar to app icon
+        let topColor = UIColor(red: 234/255, green: 88/255, blue: 43/255, alpha: 1)
+        let bottomColor = UIColor(red: 138/255, green: 75/255, blue: 39/255, alpha: 1)
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
 
     override func didReceiveMemoryWarning() {
