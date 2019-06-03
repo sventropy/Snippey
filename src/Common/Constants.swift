77d3e0efbc07716cbe241cf048fe7c8af951a92a
sventropy <sventropy@mailbox.org>
2019-06-03 23:33:56 +0200

    moved app to new bundle id and dev team

--- src/Common/Constants.swift
+++ src/Common/Constants.swift
@@ -17,7 +17,7 @@ struct Constants {
     static let keyboardBackgroundColor = UIColor(displayP3Red: 199/255, green: 203/255, blue: 210/255, alpha: 1)
     static let buttonBackgroundColor = UIColor.white
     static let textColor = UIColor.black
-    static let appGroup = "group.de.sventropy.snippey"
+    static let appGroup = "group.de.sventropy.app.snippey"
     static let defaultsSnippetsKey = "snippets"
     static let textAreaSideInset : CGFloat = 5.0
     static let textAreaTopBottomInset : CGFloat = 8.0
