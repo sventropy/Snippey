0c5ee446339667aca74a9de1ae0299f8b6b6d3e1
sventropy <sventropy@mailbox.org>
2019-06-06 12:14:51 +0200

    fixed missing initializaer implementation

--- src/Common/SnippetTableViewCell.swift
+++ src/Common/SnippetTableViewCell.swift
@@ -27,7 +27,7 @@ class SnippetTableViewCell : UITableViewCell {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
     // MARK: - View Lifecycle
