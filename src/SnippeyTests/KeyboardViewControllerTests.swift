b02e9b84e780bdcefc81bc045f821c28b0c2b0c0
sventropy <sventropy@mailbox.org>
2019-06-06 09:04:52 +0200

    removed obsolete utils class and related tests, introduced new test classes and planned test case implementation (comments)

---
+++ src/SnippeyTests/KeyboardViewControllerTests.swift
@@ -0,0 +1,38 @@
+//
+//  KeyboardViewControllerTests.swift
+//  SnippeyTests
+//
+//  Created by Hennessen, Sven on 06.06.19.
+//  Copyright © 2019 Hennessen, Sven. All rights reserved.
+//
+
+import XCTest
+
+class KeyboardViewControllerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    //TODO
+    // test number of items displayed is correct
+    // test no items loaded
+    // test add button pressed
+
+}
--- src/SnippeyTests/SnippeyTests.swift
+++ src/SnippeyTests/SnippeyTests.swift
@@ -7,7 +7,6 @@
 //
 
 import XCTest
-import UIKit
 
 class SnippeyTests: XCTestCase {
 
@@ -17,41 +16,5 @@ class SnippeyTests: XCTestCase {
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func test_util_findFirstSubviewOfTypeRecursive_findSimpleSubview_Success() {
-        let button = UIButton()
-        let view = UIView()
-        view.addSubview(button)
-        let viewFound = Util.findFirstSubviewOfTypeRecursive(view: view, targetType: UIButton.self)
-        XCTAssertEqual(button, viewFound)
-    }
-    
-    func test_util_findFirstSubviewOfTypeRecursive_findInMultipleSubviews_Success() {
-        let button = UIButton()
-        let label = UILabel()
-        let textField = UITextField()
-        let view = UIView()
-        view.addSubview(label)
-        view.addSubview(button)
-        view.addSubview(textField)
-        let viewFound = Util.findFirstSubviewOfTypeRecursive(view: view, targetType: UIButton.self)
-        XCTAssertEqual(button, viewFound)
-    }
-    
-    func test_util_findFirstSubviewOfTypeRecursive_findTargetIsParameter_Success() {
-        let button = UIButton()
-        let viewFound = Util.findFirstSubviewOfTypeRecursive(view: button, targetType: UIButton.self)
-        XCTAssertEqual(button, viewFound)
-    }
-
-    func test_util_findFirstSubviewOfTypeRecursive_findNoMatch_Fail() {
-        let label = UILabel()
-        let textField = UITextField()
-        let view = UIView()
-        view.addSubview(label)
-        view.addSubview(textField)
-        let viewFound = Util.findFirstSubviewOfTypeRecursive(view: view, targetType: UIButton.self)
-        XCTAssertNil(viewFound)
     }
 }
